66644f75abd494f970be251f6546dfd72ecc61e3
joel <joelcrawford30@gmail.com>
2021-05-17 15:04:53 +0300

    Vue js

--- app.js
+++ app.js
@@ -7,10 +7,24 @@ const app = Vue.createApp({
 			lastName: 'Joel',
 			email: 'joelcrawford@gmail.com',
 			gender: 'male',
-			picture: 'https://randomuser.me/api/portraits/men/10.jpg'
+			picture: 'https://randomuser.me/api/portraits/men/10.jpg',
 		}
 
-	}
+	},
+
+	methods: {
+		async getUser(){
+			const res  = await  fetch('https://randomuser.me/api')
+			const { results } = await res.json()
+			//console.log(results)
+			this.firstName =  results[0].name.first
+			this.lastName = results[0].name.last
+			this.email = results[0].email
+			this.gender = results[0].gender
+			this.picture = results[0].picture.large
+
+	},
+},
 })
 
 
